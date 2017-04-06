ddcd2d8e4db36781405df2c6b120a797d8c24923
Greg Doud <gpdoud@gmail.com>
2017-04-06 08:45:56 -0400

    initial commit

---
+++ TestConnectionString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConnectionString {
+	class Program {
+		static void Main(string[] args) {
+			var connStr = @"<connection string goes here>";
+			System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(connStr);
+			conn.Open();
+			if(conn.State != System.Data.ConnectionState.Open) {
+				Console.WriteLine("Connection did not open!");
+			} else {
+				Console.WriteLine("Connection open successful!");
+			}
+			Console.WriteLine("press any key to end ...");
+			Console.ReadKey();
+		}
+	}
+}
